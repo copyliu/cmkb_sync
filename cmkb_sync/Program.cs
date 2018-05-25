ddbbb0866788093d06872496f0794056e0ae5bbf
Copy Liu <copyliu@gmail.com>
2018-05-25 23:39:43 +0800

    加入專案檔案。

---
+++ cmkb_sync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cmkb_sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
